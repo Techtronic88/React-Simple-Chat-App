1d6a7b2f4793e1f18c7ac68378ca1d946f2998ba
DoanThomas88 <38048770+Techtronic88@users.noreply.github.com>
2019-05-05 23:36:08 +0700

    Add files via upload
    Created Dummy Data Objects to represent senderID and message text
    Organized the architecture of the main App components / Child Components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import MessageList from "./components/MessageList.js"
+import SendMessageForm from "./components/SendMessageForm.js"
+import RoomList from "./components/RoomList.js"
+import NewRoomForm from "./components/NewRoomForm.js"
+import './App.css';
+
+class App extends Component {
+  render() {
+  return (
+    <div className="app">
+      
+      <MessageList />
+      <SendMessageForm />
+      <RoomList />
+      <NewRoomForm />
+
+    </div>
+  );
+ }
+}
+
+export default App;
---
+++ src/components/MessageList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+const DUMMY_DATA = [
+  {
+  senderId: 'perborgen',
+  text: 'Hey, how is it going?'
+},
+{
+  senderId: 'janedoe',
+  text: 'Yer good and you'
+},
+{
+  senderId: 'perborgen',
+  text: 'Good to hear! I am good as well'
+}
+
+]   
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default MessageList
